34378360e6e8b31c40fbee47f3460a41907dc27a
Akshay <akshayt3611@gmail.com>
2025-05-31 01:27:44 +0530

    edited header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
 
 const header = () => {
   return (
-    <div>
-      <div>
+    <div className="flex justify-between">
+      <div className="flex items-center justify-center gap-2">
         <img src="..\Assets\Logo.svg" alt="logo"></img>
+        <button className="bg-gradient-to-r from-orange-500 to bg-red-500 text-xs text-white py-1.5 px-2.5 rounded-2xl">
+          Hostinger is Hiring
+        </button>
       </div>
-      <div></div>
+      <div>
+        <FaBars></FaBars>
+      </div>
     </div>
   );
 };
