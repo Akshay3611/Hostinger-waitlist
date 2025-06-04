df0b03d9d96addf7297f6f5b2e1d0db2ae11b236
Akshay <akshayt3611@gmail.com>
2025-06-05 01:05:38 +0530

    added hero image in body file

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,21 +4,25 @@ function body() {
   return (
     <div>
       <div className="flex items-center justify-center">
-        <img src="..\Assets\Blue Shape.svg" alt="1st layer" className=""></img>
+        <img
+          src="..\Assets\Blue Shape.svg"
+          alt="1st layer"
+          className="-rotate-45 h-64"
+        ></img>
         <img
           src="..\Assets\Pink Shape.svg"
           alt="2nd layer"
-          className="absolute"
+          className="absolute -rotate-35 h-64"
         ></img>
         <img
           src="..\Assets\Purple Shape.svg"
           alt="3rd  layer"
-          className="absolute"
+          className="absolute -rotate-15 h-64"
         ></img>
         <img
           src="..\Assets\Hero Image (Model).png"
           alt="hero"
-          className="absolute"
+          className="absolute h-64"
         ></img>
       </div>
       <div></div>
