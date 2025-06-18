a68450bb939b0efc90db082fd7f39e52368a2d94
Akshay <akshayt3611@gmail.com>
2025-06-18 23:34:59 +0530

    trying to deploy on githubpages

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function body() {
   return (
-    <div className="space-y-4 lg:flex ">
-      <div className="flex items-center justify-center lg:flex-1 lg:order-last">
+    <div className="space-y-4 lg:flex">
+      <div className="flex items-center justify-center lg:flex-1 lg:order-2">
         <img
           src="..\Assets\Blue Shape.svg"
           alt="1st layer"
@@ -25,11 +25,11 @@ function body() {
           className="absolute h-64 md:h-72 lg:h-130"
         ></img>
       </div>
-      <div className="lg:flex-1 lg:xl">
+      <div className="lg:flex-1 lg:order-1">
         <h1 className="text-5xl font-bold font-Playfair leading-tight lg:text-7xl">
           Host your Website in less than 5 minutes
         </h1>
-        <p1 className="font-lato text-gray-400 lg:text-2xl">
+        <p1 className="font-lato text-gray-400 lg:text-xl">
           With Hoster , get your website up and running in no less than the 5
           minutes with the most competitive pricing packahe available online
         </p1>
